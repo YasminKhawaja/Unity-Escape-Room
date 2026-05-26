e94a6f42eaeee9ca771a891192b83516cc698e4e
R1040604 <r1040604@student.thomasmore.be>
2026-05-26 15:02:43 +0200

    Kist script ingevuld

--- Assets/BrokenVector/Escape_Dungeon/Scripts_by_Yasmin/TreasureKistController.cs
+++ Assets/BrokenVector/Escape_Dungeon/Scripts_by_Yasmin/TreasureKistController.cs
@@ -3,14 +3,48 @@ using UnityEngine;
 public class TreasureKistController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    [SerializeField] private RoomPuzzleState roomPuzzleState;
+    [SerializeField] private KeyPickUpInteractable keyPickupInteractable;
+    [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource openAudioSource;
+    [SerializeField] private Light chestFeedbackLight;
+
+    private bool isUnlocked;
+    private bool isOpened;
+
+    public void Interact()
     {
-        
+        if (!isUnlocked || isOpened)
+        {
+            return;
+        }
+
+        isOpened = true;
+        roomPuzzleState.OpenChest();
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Open");
+        }
+
+        if (openAudioSource != null)
+        {
+            openAudioSource.Play();
+        }
+
+        if (chestFeedbackLight != null)
+        {
+            chestFeedbackLight.intensity = 4f;
+        }
+
+        keyPickupInteractable.UnlockKey();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnlockChest()
     {
-        
+        isUnlocked = true;
     }
 }
+
