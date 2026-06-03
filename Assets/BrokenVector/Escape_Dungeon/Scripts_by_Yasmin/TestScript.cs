88622559d8323f2e7c6b38c0861e33d49c957ffe
R1040604 <r1040604@student.thomasmore.be>
2026-06-03 10:13:29 +0200

    update: script

--- Assets/BrokenVector/Escape_Dungeon/Scripts_by_Yasmin/TestScript.cs
+++ Assets/BrokenVector/Escape_Dungeon/Scripts_by_Yasmin/TestScript.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    private Animator animator;
+    private Animator mAnimator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponent<Animator>();
+        mAnimator = GetComponent<Animator>();
         Debug.Log("TEST WERKT op: Treasure_Chest_Base_02");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (animator != null)
+        if (mAnimator != null)
         {
             if (Input.GetKeyDown(KeyCode.O))
             {
-                animator.SetTrigger("Open");
+                mAnimator.SetTrigger("Open");
             }
         }
     }
